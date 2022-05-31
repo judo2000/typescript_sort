a94e9c8d7aa89cc47da38d38d1bf92b9d5c58d66
Stephen Moore <judo2000@comcast.net>
2022-05-31 15:33:26 -0600

    Initial commit

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('123');
+const logSomething = () => {
+    console.log('so1mething');
+};
+logSomething();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log('123');
+
+const logSomething = () => {
+  console.log('so1mething');
+};
+
+logSomething();
