97c1839116f7084a6c5ceba6010686b3c19bc577
Stephen Moore <judo2000@comcast.net>
2022-06-01 17:44:02 -0600

    Create interface to define properties required for sorting algorythm and then create CharactersCollection.ts

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,7 +1,11 @@
-import { NumbersCollection } from './NumbersCollection';
+interface Sortable {
+  length: number;
+  compare(leftINdex: number, rightINdex: number): boolean;
+  swap(leftINdex: number, rightINdex: number): void;
+}
 
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
     const { length } = this.collection;
