3051fd86dbeff34dc94760af06696d81b7f661e4
Stephen Moore <judo2000@comcast.net>
2022-06-03 07:58:30 -0600

    Convert LinkedList and CharactersCollection classes to work with the abstract Sorter class

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
 // const charactersCollection = new CharactersCollection('Xaayb');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
+// charactersCollection.sort();
 // console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -19,6 +17,5 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
